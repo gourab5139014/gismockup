a76aa88e5f32824e17c3fe67e348fe897371ec71
Gourab Mitra <gourabmi@buffalo.edu>
2017-11-28 01:25:32 -0500

    remove dead code

--- assets/js/src/gourab.js
+++ assets/js/src/gourab.js
@@ -23,22 +23,3 @@ window.onload = function () { // wait for load in a dumb way because B-0
         // window.location.href = 'Students.html';
     })
 }
-
-window.onload = function searchBox() {
-    // Declare variables
-    var input, filter, ul, li, a, i;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}

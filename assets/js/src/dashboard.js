188aef5a65687fc32f7b42a9d1e9f4a3b0c62af2
Gourab Mitra <gourabmi@buffalo.edu>
2017-12-02 18:27:45 -0500

    Introduced search in dataset display

--- assets/js/src/dashboard.js
+++ assets/js/src/dashboard.js
@@ -24,7 +24,8 @@
 //     })
 // }
 
-$( "#myInput" ).keyup(function() {
+// Old Search Function
+$("#xmyInput").keyup(function () {
     // Declare variables
     var input, filter, ul, li, a, i;
     input = document.getElementById('myInput');
@@ -42,3 +43,25 @@ $( "#myInput" ).keyup(function() {
         }
     }
 });
+
+$("#myInput").keyup(function () {
+    // Declare variables 
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+});
+
